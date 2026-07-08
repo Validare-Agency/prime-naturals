f8b83496c412a380f92b9d3ee160c34bcb3c1d1a
M-Saimm <muhammad@validareagency.com>
2026-07-08 14:57:54 +0500

    Ab test/v prime pdp 10 (#9)
    * V_PRIME_PDP_10
    
    * Var updated

--- assets/c-intelligems-tests.js
+++ assets/c-intelligems-tests.js
@@ -40,6 +40,17 @@ function handleExperiments() {
   if (primeHp05?.name === "Var A") {
     document.body.classList.add("c-primeHp05VarA");
   }
+
+  // Test: V_PRIME_PDP_10 | Gallery Images Optimization
+  const primePdp10 = window.igData?.user.getTestGroup(
+    "5bacdd65-f916-4413-815f-bc4401969b33"
+  );
+
+  if (primePdp10?.name === "Var A : Sparks Curiosity") {
+    document.body.classList.add("c-primePdp10VarA");
+  } else if (primePdp10?.name === "Var B : Gift + Less Screen Time") {
+    document.body.classList.add("c-primePdp10VarB");
+  }
 }
 
 let cartDrawerWasActive = false;
