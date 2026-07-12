0606c279892662638804da043514d96b0a6ef5cf
M-Saimm <muhammad@validareagency.com>
2026-07-12 16:52:45 +0500

    Test 02 removed

--- assets/c-intelligems-tests.js
+++ assets/c-intelligems-tests.js
@@ -4,17 +4,6 @@ let igReady = false;
 function handleExperiments() {
   if (!domLoaded || !igReady) return;
 
-  // Test: V_PRIME_CART_02 - Cart Trust Assurance Policies
-  const primeCart02 = window.igData?.user.getTestGroup(
-    "f2abbdfd-8369-4f44-8427-22337bacae56"
-  );
-
-  if (primeCart02?.name === "Var A") {
-    document.body.classList.add("c-primeCart02VarA");
-  } else {
-    document.body.classList.add("c-primeCart02Control");
-  }
-
   // Test: V_PRIME_PDP_07 | PDP review format: auto-scroll vs manual arrows
   const primePdp07 = window.igData?.user.getTestGroup(
     "ec0d5412-f677-4267-b944-7cb74ffda215"
