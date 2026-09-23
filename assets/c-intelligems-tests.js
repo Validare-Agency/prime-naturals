dddfa0b69b25f07ced0b020f34426204dbb1dc25
M-Saimm <muhammad@validareagency.com>
2026-09-23 15:26:11 +0500

    test

--- assets/c-intelligems-tests.js
+++ assets/c-intelligems-tests.js
@@ -82,7 +82,8 @@ function kachingAttemptSwap(attempt) {
 
 // Validare Holdout. Permanent, never end it.
 const HOLDOUT_EXPERIMENT_ID = "3ad2181f-d285-418c-b4d8-a52ce3a136a1";
-const HOLDOUT_GROUP_ID = "c41ea5b4-45b8-4edf-8687-844be31c4054";
+// const HOLDOUT_GROUP_ID = "c41ea5b4-45b8-4edf-8687-844be31c4054";
+const HOLDOUT_GROUP_ID = "213";
 
 function handleExperiments() {
   if (!domLoaded || !igReady) return;
@@ -135,11 +136,35 @@ function handleExperiments() {
       !primePdp35InVarAOrB
   );
 
+<<<<<<< Updated upstream
   // Test: V_PRIME_CART_32 | Charity Donation Minicart Add-On
   const primeCart32 = window.igData?.user.getTestGroup(
     "09ae0d10-fdd8-4c8b-91e8-e282765ad1a2"
   );
   if (primeCart32?.name === "Var A - Opt-in") {
+=======
+  // Test: V_PRIME_PDP_27 | Physical-Size Information - "Exactly What Arrives"
+  const primePdp27 = window.igData?.user.getTestGroup(
+    "ad3c1130-56a6-4aaa-a8bc-d22ebcc2cc64"
+  );
+  if (primePdp27?.id === "7d0e0615-de58-4c8c-a2fc-425fddb31f8d") {
+    document.body.classList.add("c-primePdp27VarA");
+  } else if (primePdp27?.id === "c82a6023-4e4c-4d51-8de0-3ed8db9f16ce") {
+    document.body.classList.add("c-primePdp27VarB");
+  } else if (primePdp27?.id === "e871cc71-7f5e-4756-9df6-a65c7e55b194") {
+    document.body.classList.add("c-primePdp27VarC");
+  } else if (primePdp27?.id === "8fdee977-6c36-4379-bbbb-4bd83be59175") {
+    document.body.classList.add("c-primePdp27VarD");
+  } else if (primePdp27?.id === "36f59724-5e21-411e-94c1-8988cfd83384") {
+    document.body.classList.add("c-primePdp27VarE");
+  } else if (primePdp27?.id === "640a45aa-d390-42b2-a8a8-28d5f305a890") {
+    document.body.classList.add("c-primePdp27VarF");
+  }
+
+  // Test: V_PRIME_CART_32 | Charity Donation Minicart Add-On — Give the Gift of Reading (BFCM)
+  const primeCart32 = { id: "bc53f2c0-d5e5-4829-91b5-298e23b0a3b6" }
+  if (primeCart32?.id === "bc53f2c0-d5e5-4829-91b5-298e23b0a3b6") {
+>>>>>>> Stashed changes
     document.body.classList.add("c-primeCart32VarA");
   } else if (primeCart32?.name === "Var B - Preselected") {
     document.body.classList.add("c-primeCart32VarB");
